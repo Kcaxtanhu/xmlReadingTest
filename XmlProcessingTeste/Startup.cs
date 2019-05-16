16c4dcb7eecf8047a60615aa7aafe4cc00acf8ea
Damásio Sabino <dsabino@primeit.pt>
2019-05-16 18:26:34 +0100

    Trying to add Swagger

--- XmlProcessingTeste/Startup.cs
+++ XmlProcessingTeste/Startup.cs
@@ -3,6 +3,12 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
 
 namespace XmlProcessingTeste
 {
@@ -20,6 +26,36 @@ namespace XmlProcessingTeste
         {
             services.AddMvc(o => o.InputFormatters.Insert(0, new RawTextFormatter()))
                     .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc(
+                    "v1",
+                    new Info
+                    {
+                        Title = "Teste XML Performance",
+                        Version = "v1",
+                        Description = "Não sei o que escrever..."
+                    });
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+
+                c.AddSecurityRequirement(new Dictionary<string, IEnumerable<string>> {
+                    { "Bearer", Enumerable.Empty<string>() },
+                });
+
+
+                c.AddSecurityDefinition("Bearer", new ApiKeyScheme
+                {
+                    Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+                    Name = "Authorization",
+                    In = "header",
+                    Type = "apiKey"
+                });
+
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,6 +73,17 @@ namespace XmlProcessingTeste
 
             app.UseHttpsRedirection();
             app.UseMvc();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint($"/XmlProcessingTeste/swagger/v1/swagger.json", "XmlProcessingTeste");
+                c.RoutePrefix = string.Empty;
+            });
         }
     }
 }
